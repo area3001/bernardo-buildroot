581e9d4f7a55363032f5ebf2edb2963a4aebd5b6
Bert Outtier <outtierbert@gmail.com>
2017-12-17 12:32:04 +0100

    bernardo overlay is compiled

--- board/bernardo/post-image.sh
+++ board/bernardo/post-image.sh
@@ -5,8 +5,15 @@ BOARD_DIR="$(dirname $0)"
 # copy the uEnv.txt to the output/images directory
 cp ${BOARD_DIR}/uEnv.txt $BINARIES_DIR/uEnv.txt
 
+mkdir -p ${BINARIES_DIR}/overlays
+
 # compile and copy self-defined overlays
 DTC=`ls ${BUILD_DIR}/linux-*/scripts/dtc/dtc | head -n1`
+CPP=${HOST_DIR}/bin/arm-linux-cpp
+LINUX_INCLUDE=`echo ${BUILD_DIR}/linux-*/include | head -n1 | awk '{print $1;}'`
+
+rm -rf ${BINARIES_DIR}/overlays/*
+
 if ! [ -x $DTC ]; then
 	DTC=dtc
 else
@@ -15,10 +22,10 @@ fi
 
 for DTS in ${BOARD_DIR}/*-overlay.dts; do
 	DTSNAME=`basename ${DTS%%-overlay.dts}`
-	echo "Compile $DTSNAME"
-	$DTC -O dtb $DTS -o ${BINARIES_DIR}/${DTSNAME}.dtbo
+	echo "Compile $DTSNAME ${LINUX_INCLUDE}"
+    $CPP -nostdinc -I${LINUX_INCLUDE} -undef -x assembler-with-cpp $DTS > ${BINARIES_DIR}/overlays/${DTSNAME}.tmp.dts
+    $DTC -@ -O dtb ${BINARIES_DIR}/overlays/${DTSNAME}.tmp.dts -o ${BINARIES_DIR}/overlays/${DTSNAME}.dtbo
 done
-
 
 GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
