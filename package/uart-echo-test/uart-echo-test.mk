f9c4d5b9ce2250b03da610d0184e058970f992f3
Bert Outtier <outtierbert@gmail.com>
2018-04-11 20:24:34 +0200

    Added uart sending test application and enable rs485 application

--- package/uart-echo-test/uart-echo-test.mk
+++ package/uart-echo-test/uart-echo-test.mk
@@ -4,13 +4,23 @@
 #
 ################################################################################
 
+TARGET_CFLAGS += -c -Wall -O2
+
 define UART_ECHO_TEST_BUILD_CMDS
-	$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS) \
-		$(UART_ECHO_TEST_PKGDIR)/main.c -o $(@D)/uart-echo-test
+	$(TARGET_CC) $(TARGET_CFLAGS) \
+		$(UART_ECHO_TEST_PKGDIR)/main.c -o $(@D)/uart-echo-test.o
+
+	$(TARGET_CC) $(@D)/uart-echo-test.o -o $(@D)/uart-echo-test
+
+	$(TARGET_CC) $(TARGET_CFLAGS) \
+		$(UART_ECHO_TEST_PKGDIR)/send.c -o $(@D)/uart-send-test.o
+
+	$(TARGET_CC) $(@D)/uart-send-test.o -o $(@D)/uart-send-test
 endef
 
 define UART_ECHO_TEST_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 755 $(@D)/uart-echo-test $(TARGET_DIR)/usr/sbin/uart-echo
+	$(INSTALL) -D -m 755 $(@D)/uart-echo-test $(TARGET_DIR)/usr/bin/uart-echo
+	$(INSTALL) -D -m 755 $(@D)/uart-send-test $(TARGET_DIR)/usr/bin/uart-send
 endef
 
 $(eval $(generic-package))
